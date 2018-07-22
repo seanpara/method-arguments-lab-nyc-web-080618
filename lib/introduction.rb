4b7411f708c7c9a4826aab0dbd5cd815c3b4d2b7
Sean Logan Para <spara77@gmail.com>
2018-07-22 20:36:04 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,6 @@ end
 
 
 def introduction_with_language(name, language)
-   puts "Hi, my name is #{name} and I am learning to program with #{language}."
+   puts "Hi, my name is  and I am learning to program with #{language}."
+   puts "Hi, my name is #{name} and I am learning to program with Ember.js." 
 end 
