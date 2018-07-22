7fb43a39a144d2ad5d42abaa21798a3e2734235b
Sean Logan Para <spara77@gmail.com>
2018-07-22 20:38:17 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction(name)
 end
 
 
-def introduction_with_language(name, language)
+def introduction_with_language(name,language)
    puts "Hi, my name is #{name} and I am learning to program with #{language}." 
 end 
